b93d8c682a01b8ff51c592d7c2df890c10e523cd
mayur9093 <mayurpatilr9@gmail.com>
2026-08-05 10:24:17 +0530

    first commit

---
+++ TNSIFJAVA/src/org/tnsif/acc/c2tc/Scanner/Advance_Scanner.java
@@ -0,0 +1,32 @@
+package TNSIFJAVA.src.org.tnsif.acc.c2tc.Scanner;
+import java.util.Scanner;
+public class Advance_Scanner {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter some data(you can numbers or text)");
+		
+		while(scan.hasNext())
+		{
+			if(scan.hasNextInt())
+			{
+				 int intValue=scan.nextInt();
+				 System.out.println("Read integer : " + intValue);
+				 scan.nextLine();
+			}
+			else if(scan.hasNextDouble())
+			{
+				double doubleValue =scan.nextDouble();
+				System.out.println("Read a double "+doubleValue);
+				scan.nextLine();
+			}
+			else
+			{
+				String stringvalue =scan.next();
+				System.out.println("Unkown input"+ stringvalue);
+			}
+		}
+        
+       scan.close();
+	}
+
+}
