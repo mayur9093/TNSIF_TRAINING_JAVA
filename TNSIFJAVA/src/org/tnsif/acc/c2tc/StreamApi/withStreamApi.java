01a01a9fe1c1938d0d3f0d181c9c97744a614e98
mayur9093 <mayurpatilr9@gmail.com>
2026-08-24 12:47:43 +0530

    first commit

---
+++ TNSIFJAVA/src/org/tnsif/acc/c2tc/StreamApi/withStreamApi.java
@@ -0,0 +1,19 @@
+package TNSIFJAVA.src.org.tnsif.acc.c2tc.StreamApi;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class withStreamApi {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+
+        List<Integer> result = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .map(n -> n * 2)
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println(result);
+    }
+}
